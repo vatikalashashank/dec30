5d68bb8abfb50d80b4a61a7b8ef7ec1e5f11e0c1
vatikalashashank <vatikalashashank@gmail.com>
2024-12-30 09:00:55 +0530

    Create val=[1,3,4,5,6,7].py

---
+++ val=[1,3,4,5,6,7].py
@@ -0,0 +1,13 @@
+# val=[1,3,4,5,6,7]
+# # op:[1,3,5,7]
+# emp_lst=[]
+# for x in val:
+#     if x%2==1:
+#         emp_lst.append(x)
+#     else:
+#         pass
+#     print(emp_lst)
+
+# print([x for x in [1,3,4,5,6,7]if x%2==1])
+# val=[1,2,3]
+# print([f'even value{x}'if x%2==0 else f'{x}odd value'for x in val])
